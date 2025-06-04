b8aab2b620a381c5c133a5e293c16fbbbac8db9b
Ralph-DnG <ralphmaronilla003@gmail.com>
2025-06-04 14:19:07 +0800

    update
    update on material ui setup and theming, added sidebar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,42 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
-import App from "./App.jsx";
+import App from './App.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
+    // children: [
+    //   {
+    //     path: '/login',
+    //     element: <Login />,
+    //     errorElement: <ErrorPage />,
+    //   },
+    //   {
+    //     path: '/register',
+    //     element: <Register />,
+    //     errorElement: <ErrorPage />,
+    //   },
+    // ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
-const root = document.getElementById("root");
+const root = document.getElementById('root');
 
 ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import * as Yup from 'yup';
+import {
+  Box,
+  FormControl,
+  TextField,
+  Typography,
+  useTheme,
+} from '@mui/material';
+import Button from '@mui/material/Button';
+import { Formik } from 'formik';
+
+const loginInitialValues = {
+  email: '',
+  password: '',
+};
+
+const loginValidationSchema = Yup.object().shape({
+  email: Yup.string().required('Email is required'),
+  password: Yup.string().required('Password is required'),
+});
+
+const Login = () => {
+  const theme = useTheme();
+  const handleLoginSubmit = async (values) => {
+    console.log('Login submitted:', values);
+  };
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Formik
+        initialValues={loginInitialValues}
+        validationSchema={loginValidationSchema}
+        onSubmit={handleLoginSubmit}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '1rem',
+                backgroundColor: theme.palette.background.default,
+                height: '500px',
+                width: '700px',
+                padding: '1rem 3rem',
+                justifyContent: 'center',
+                borderRadius: '1rem',
+              }}
+            >
+              <Typography
+                variant='h3'
+                sx={{
+                  color: '#333',
+                  textAlign: 'center',
+                  fontWeight: 'bold',
+                  marginY: '1rem',
+                }}
+              >
+                Indoor OOH
+              </Typography>
+              <FormControl>
+                <TextField
+                  label='Email'
+                  name='email'
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.email && Boolean(errors.email)}
+                  helperText={touched.email && errors.email}
+                />
+              </FormControl>
+              <FormControl>
+                <TextField
+                  label='Password'
+                  name='password'
+                  type='password'
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.password && Boolean(errors.password)}
+                  helperText={touched.password && errors.password}
+                />
+              </FormControl>
+
+              <Button
+                type='submit'
+                variant='contained'
+                sx={{
+                  backgroundColor: '#ea5b20',
+                  color: '#fff',
+                  width: '200px',
+                }}
+              >
+                Login
+              </Button>
+            </Box>
+          </form>
+        )}
+      </Formik>
+    </Box>
+  );
+};
+
+export default Login;
