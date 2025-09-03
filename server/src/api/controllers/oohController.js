02036190b8946f3ee4cd592552af2fa767c04e0a
JM-DnG <josemari.ronquillo@davidandgolyat.com>
2025-09-03 13:38:11 +0800

    Update oohController.js

--- server/src/api/controllers/oohController.js
+++ server/src/api/controllers/oohController.js
@@ -6,10 +6,10 @@ export async function getOoh(req, res) {
     const ooh = await OohInventory.query()
       .withGraphJoined("[address, dimensions, details.discountedRate]")
       .modifyGraph("address", (builder) => {
-        builder.select("street", "city", "province", "country", "country_code", "postcode");
+        builder.select("street", "city", "province", "country", "country_code", "postcode","landmark");
       })
       .modifyGraph("dimensions", (builder) => {
-        builder.select("height_in_ft", "width_in_ft", "diagonal_in_ft", "radius_in_meters");
+        builder.select("height_in_ft", "width_in_ft", "diagonal_in_ft", "radius_in_meters","facing");
       })
       .modifyGraph("details", (builder) => {
         builder.select("traffic", "inclusions", "traffic_count");
