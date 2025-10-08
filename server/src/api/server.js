e634caf2d82c67742666877713e210bcae1cbc75
JM-DnG <josemari.ronquillo@davidandgolyat.com>
2025-10-08 13:47:34 +0800

    for prod fix

--- server/src/api/server.js
+++ server/src/api/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import http from 'http';
 import path from 'path';
+require("dotenv").config({ path: path.join(__dirname, "../", "../", ".env") });
 import { fileURLToPath } from 'url';
 import app from './app.js';
 import knex from './db/database.js';  // this binds knex to Objection
@@ -8,9 +9,6 @@ import knex from './db/database.js';  // this binds knex to Objection
 // Handle __dirname in ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-// Load .env before anything else that relies on process.env
-dotenv.config({ path: path.join(__dirname, '..', '..', '.env') });
 
 const port = process.env.PORT_PROJECT_INDOOR_OOH || 3000;
 const server = http.createServer(app);
